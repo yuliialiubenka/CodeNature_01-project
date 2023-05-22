c9c62d9131f86c0170a42dfaa0baf7426a3d6daa
Yuliia Liubenka <yuliialiubenka@gmail.com>
2023-05-22 19:15:39 +0300

    Header mobile-menu
    Header mobile-menu changes,
    adding new basket icon

--- src/main.js
+++ src/main.js
@@ -38,6 +38,7 @@ window.addEventListener('DOMContentLoaded', () => {
             slide.classList.add('active');
         })
     }
+
     function clearActivClasses() {
         slides.forEach((slide) => {
             slide.classList.remove('active')
@@ -47,67 +48,83 @@ window.addEventListener('DOMContentLoaded', () => {
     $('.carousel').carousel({
         interval: 3000
     })
-});
 
+    // Mobile Menu
 
-var modalButtons = document.querySelectorAll('.js-open-modal'),
+    const mobileMenu = document.querySelector('.js-menu-container');
+    const openMenuBtn = document.querySelector('.js-open-menu');
+    const closeMenuBtn = document.querySelector('.js-close-menu');
+    const body = document.querySelector('body');
+  
+    const toggleMenu = () => {
+      const isMenuOpen =
+        openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+        openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+        mobileMenu.classList.toggle('is-open');
+        body.classList.toggle('overflow-hidden');
+    };
+    openMenuBtn.addEventListener('click', toggleMenu);
+    closeMenuBtn.addEventListener('click', toggleMenu);
+
+    function hideCloseBtn() {
+        openMenuBtn.style.display = 'flex';
+        closeMenuBtn.style.display = 'none';
+    }
+    function showCloseBtn() {
+        openMenuBtn.style.display = 'none';
+        closeMenuBtn.style.display = 'flex'; 
+    }
+    openMenuBtn.addEventListener('click', () => showCloseBtn());
+    closeMenuBtn.addEventListener('click', () => hideCloseBtn());
+    
+
+    // Close the mobile menu on wider screens if the device orientation changes
+    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+      if (!e.matches) return;
+      mobileMenu.classList.remove('is-open');
+      body.classList.remove('overflow-hidden');
+      openMenuBtn.setAttribute('aria-expanded', false);
+    });
+
+    // Modals
+    var modalButtons = document.querySelectorAll('.js-open-modal'),
        overlay      = document.querySelector('.js-overlay-modal'),
        closeButtons = document.querySelectorAll('.js-modal-close');
 
+    modalButtons.forEach(function(item){
 
-   /* Перебираем массив кнопок */
-   modalButtons.forEach(function(item){
-
-      /* Назначаем каждой кнопке обработчик клика */
-      item.addEventListener('click', function(e) {
-
-         /* Предотвращаем стандартное действие элемента. Так как кнопку разные
-            люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
-            Нужно подстраховаться. */
-         e.preventDefault();
-
-         /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
-            и будем искать модальное окно с таким же атрибутом. */
-         var modalId = this.getAttribute('data-modal'),
-             modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
-
-
-         /* После того как нашли нужное модальное окно, добавим классы
-            подложке и окну чтобы показать их. */
-         modalElem.classList.add('active');
-         overlay.classList.add('active');
-      }); // end click
-
-   }); // end foreach
-
-
-   closeButtons.forEach(function(item){
-
-      item.addEventListener('click', function(e) {
-         var parentModal = this.closest('.modal');
-
-         parentModal.classList.remove('active');
-         overlay.classList.remove('active');
+        item.addEventListener('click', function(e) {
+            e.preventDefault();
+            var modalId = this.getAttribute('data-modal'),
+            modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
+            modalElem.classList.add('active');
+            overlay.classList.add('active');
       });
+    });
 
-   }); // end foreach
+    closeButtons.forEach(function(item){
 
+        item.addEventListener('click', function(e) {
+        var parentModal = this.closest('.modal');
+
+        parentModal.classList.remove('active');
+        overlay.classList.remove('active');
+        });
+    });
 
     document.body.addEventListener('keyup', function (e) {
         var key = e.keyCode;
-
         if (key == 27) {
-
             document.querySelector('.modal.active').classList.remove('active');
             document.querySelector('.overlay').classList.remove('active');
         };
     }, false);
 
-
     overlay.addEventListener('click', function() {
         document.querySelector('.modal.active').classList.remove('active');
         this.classList.remove('active');
     });
+});
 
 
 
